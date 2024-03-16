47fbb7a0733cc54465a7c9b1a04542cd19b4dc16
ricardo <nocive.science@gmail.com>
2024-03-15 23:28:03 -0300

    funciona bien pero sin become

--- fourier_scene.py
+++ fourier_scene.py
@@ -1,17 +1,29 @@
 from manim import *
-
-class RotatingVector(Scene):
+class VectorFourierScene(Scene):
+    config={
+        'config_vector': {
+            'color': BLUE,
+            'max_tip_length_to_length_ratio': 0.25,
+            'tip_length': 0.25,
+            'stroke_width': 1.2
+        },
+    }
     def construct(self):
-        self.time = 0
-
-        vector = Vector(RIGHT, color=BLUE)
-        vector2= vector.copy()
-        vector2.add_updater(lambda v: v.move_to(vector.get_end(), v.get_start()))
-        vector.add_updater(self.rotate_vector)
-
-        self.add(vector, vector2)
-        self.wait(3)
-
-    def rotate_vector(self, vector, dt):
-        self.time += dt
-        vector.become(Vector(RIGHT, color=BLUE).rotate(self.time, about_point=vector.get_start()))
+        vector=self.get_rotating_vector()
+        self.add(vector)
+        self.wait(2)
+    def get_rotating_vector(self, freq, coef, center_func):
+        vector=Vector(RIGHT,**self.config['config_vector'])
+        vector.freq=freq
+        vector.coef=coef
+        vector.center_func=center_func
+        vector.add_updater(self.get_update_rotating_vector)
+        return vector
+    def get_update_rotating_vector(self, vector, dt):
+        time=0
+        time+= dt
+        coef=vector.coef
+        freq=vector.freq
+        center_func=vector.center_func
+        phase = coef
+        vector.rotate(phase, about_point=vector.get_start())

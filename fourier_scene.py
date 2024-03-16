eedfe2f6e92e506fb6b511b17dc559d1f1476499
ricardo <nocive.science@gmail.com>
2024-03-16 15:13:12 -0300

    terminado

--- fourier_scene.py
+++ fourier_scene.py
@@ -9,53 +9,28 @@ class VectorFourierScene(Scene):
             'stroke_width': 1.2
         },
         'origin': ORIGIN,
+        'time': 0,
     }
     def construct(self):
-        self.add_vectors()
-    def add_vectors(self):
-        vectors=self.get_rotating_vectors()
-        self.add(vectors)
-        self.wait(4)
-    def get_freqs(self, n=None):
-        if n is None:
-            n= self.config['n_vectors']
-        all_freqs = list(np.arange(1,n+1))
-        all_freqs.sort(reverse=True)
-        return all_freqs
+        pass
     def get_coef(self):
-        return [complex(0) for _ in range(self.config['n_vectors'])]
-    def get_rotating_vectors(self, freqs=None, coefs=None):
-        if freqs is None:
-            freqs=self.get_freqs()
-        if coefs is None:
-            coefs=self.get_coef()
-        vectors=VGroup()
-        self.center_tracker= VectorizedPoint(self.config['origin'])
-        last_vector=None
-        for freq, coef in zip(freqs, coefs):
-            if last_vector is None:
-                center_func=lambda: self.center_tracker.get_location()
-            else:
-                center_func=lambda: last_vector.get_end()
-            vector=self.get_rotating_vector(freq, coef, center_func)
-            vectors.add(vector) 
-            last_vector=vector
-        return vectors
-    def get_rotating_vector(self, freq, coef, center_func):
-        vector=Vector(RIGHT,**self.config['config_vector'])
-        vector.scale(abs(coef))
-        if abs(coef)>0:
-            vector.set_color(self.config['config_vector']['color'])
-        vector.freq=freq
-        vector.coef=coef
-        vector.center_func=center_func
-        vector.add_updater(self.get_update_rotating_vector)
+        return 3
+    def get_freq(self):
+        return 1
+    def get_vectors(self):
+        pass
+    def get_vector(self, freq, coef, center_func):
+        vector= Vector(RIGHT, **self.config['config_vector'])
+        vector.freq= freq
+        vector.coef= coef
+        vector.center_func= center_func
+        vector.add_updater(self.get_update_vector)
         return vector
-    def get_update_rotating_vector(self, vector, dt):
-        time=0
-        time+= dt
-        coef=vector.coef
-        freq=vector.freq
-        center_func=vector.center_func
-        phase = coef
-        vector.rotate(phase, about_point=vector.get_start())
+    def get_update_vector(self, vector, dt):
+        self.config['time']+= dt
+        freq= vector.freq
+        coef= vector.coef
+        center_func= vector.center_func
+        vector.become(Vector(RIGHT, **self.config['config_vector']).rotate(coef*np.cos(freq*self.config['time'])*RIGHT))
+        vector.shift(center_func(self.config['time'])-vector.get_start())
+        vector.rotate()
